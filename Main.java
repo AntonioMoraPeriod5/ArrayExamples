af425e9ac590627bbbfd2c7a1521913222161e2b
AntonioMoraPeriod5 <43919907+AntonioMoraPeriod5@users.noreply.github.com>
2018-10-25 12:22:20 -0700

    Task 12 Done
    Task 12 Done

--- Main.java
+++ Main.java
@@ -257,7 +257,32 @@ class Main
          *  Count how many words have more than 5 letters.
          */
         System.out.println("*** Task 12 ***");
-        
+        String[] ar5 = new String[16];
+        int len5;
+        ar5[0]="Four";
+        ar5[1]="score";
+        ar5[2]="and";
+        ar5[3]="seven";
+        ar5[4]="years";
+        ar5[5]="ago";
+        ar5[6]="our";
+        ar5[7]="fathers";
+        ar5[8]="brought";
+        ar5[9]="forth";
+        ar5[10]="on";
+        ar5[11]="this";
+        ar5[12]="continent";
+        ar5[13]="a";
+        ar5[14]="new";
+        ar5[15]="nation";
+        int count5=0;
+        String s5="";
+        for (int i=0 ; i<ar5.length; i++)
+        {
+         s5=ar5[i];
+         if ((len5=s5.length()) >4);
+         count5++;
+        }
         /*
          * Task 13
          * Create an array called monsterArray of 5 Monsters.
